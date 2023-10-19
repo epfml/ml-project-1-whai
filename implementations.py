21e38c015ff4df7a8dfecbcd005e7e525bd0d9f7
alexisemizsd <alexi@sportsdynamics.eu>
2023-10-19 14:22:12 +0200

    report analysis try

--- implementations.py
+++ implementations.py
@@ -196,6 +196,7 @@ def calculate_logistic_loss(y, tx, w):
 
     assert y.shape[0] == tx.shape[0]
     assert tx.shape[1] == w.shape[0]
+    assert w.shape[1]==1, "weights not the right shape"
 
     loss = 0
     for i in range(y.shape[0]):
@@ -448,3 +449,44 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
         w, loss = learning_by_penalized_gradient(y, tx, w, gamma, lambda_)
 
     return w, loss
+
+
+def split_data(x, y, ratio, seed=1):
+    """
+    split the dataset based on the split ratio. If ratio is 0.8
+    you will have 80% of your data set dedicated to training
+    and the rest dedicated to testing. If ratio times the number of samples is not round
+    you can use np.floor. Also check the documentation for np.random.permutation,
+    it could be useful.
+
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        y: numpy array of shape (N,).
+        ratio: scalar in [0,1]
+        seed: integer.
+
+    Returns:
+        x_tr: numpy array containing the train data.
+        x_te: numpy array containing the test data.
+        y_tr: numpy array containing the train labels.
+        y_te: numpy array containing the test labels.
+
+    >>> split_data(np.arange(13), np.arange(13), 0.8, 1)
+    (array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]), array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]))
+    """
+    # set seed
+    np.random.seed(seed)
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # split the data based on the given ratio: TODO
+    n=x.shape[0]
+    indices = np.random.permutation(n)
+    index_split = int(np.floor(ratio * n))
+    index_tr = indices[:index_split]
+    index_te = indices[index_split:]
+    # create split
+    x_tr = x[index_tr]
+    x_te = x[index_te]
+    y_tr = y[index_tr]
+    y_te = y[index_te]
+    return x_tr, x_te, y_tr, y_te
